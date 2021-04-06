5943b1e27ca83500ddf088c81d027f3ca7396736
IDmitriy <idmitriy.fsd@gmail.com>
2021-04-06 23:22:34 +0300

    Демо: стилизована страница, подключены 4 слайдера

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -19,10 +19,45 @@ $(document).ready(function () {
   
 
   $('.slider-one').fsdSlider();
-/*   $('.fsd-slider2').fsdSlider();
-  $('#test-slider').fsdSlider(); */
+  $('.slider-two').fsdSlider({
+        direction: 'vertical', // horizontal or vertical
+        type: 'single', // single or double
+        popover: true,
+        scale: true,
+        min: 0,
+        max: 1000,
+        valueFrom: 50,
+        valueTo: 500,
+        step: 50,
+  });
+  $('.slider-three').fsdSlider({
+        direction: 'vertical', // horizontal or vertical
+        type: 'single', // single or double
+        popover: true,
+        scale: false,
+        min: 0,
+        max: 10,
+        valueFrom: 1,
+        valueTo: 9,
+        step: 1,
+  });
+  $('.slider-four').fsdSlider({
+        direction: 'horizontal', // horizontal or vertical
+        type: 'single', // single or double
+        popover: false,
+        scale: true,
+        min: -1,
+        max: 1,
+        valueFrom: -1,
+        valueTo: 0,
+        step: 0.1,
+  });
+
+
 
   let sliderOneType = document.getElementsByName('sliderOneType');
+
+  //тест передачи данных в слайдер
   for (let radio of sliderOneType) {
     radio.addEventListener('click', (event) => {
       const property= {
